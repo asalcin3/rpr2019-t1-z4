b51101198e7654b5a605314af8172e4554ed07a0
asalcin3 <72860745+asalcin3@users.noreply.github.com>
2021-10-22 13:54:45 +0200

    initial commit

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -2,5 +2,45 @@ package ba.unsa.etf.rpr;
 
 public class Korpa {
 
-  Artikl[] artikl;
+  Artikl[] artikli;
+  int brArtikla;
+  int max;
+
+  public Korpa(){
+    max=50;
+    brArtikla=0;
+    artikli=new Artikl[max];
+  }
+
+  public Artikl[] getArtikli() {
+    return artikli;
+  }
+  public boolean dodajArtikl(Artikl a){
+    if(brArtikla==max) return false;
+    else{
+      artikli[brArtikla++]=a;
+      return true;
+    }
+  }
+
+  public Artikl izbaciArtiklSaKodom(String kod){
+    Artikl n= null;
+    for(int i = 0 ; i < artikli.length ;i++){
+      if(artikli[i]!=null && artikli[i].getKod().equals(kod)){
+        n=artikli[i];
+        artikli[i]=null;
+        break;
+      }
+    }
+    return n;
+  }
+
+  public int dajUkupnuCijenuArtikala(){
+    int cijena = 0;
+    if(brArtikla== 0) return 0;
+    for(int i =0; i < brArtikla ; i++){
+      cijena = cijena + artikli[i].getCijena();
+    }
+    return cijena;
+  }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+  private Artikl[] artikli;
+  private int brArtikla;
+  private int max;
+
+  public Supermarket(){
+    artikli=new Artikl[max];
+    brArtikla=0;
+    max=1000;
+  }
+
+  public boolean dodajArtikl (Artikl biciklo){
+    if(brArtikla==max) return false;
+    else {
+      artikli[brArtikla++] = biciklo;
+      return true;
+    }
+  }
+  public Artikl[] getArtikli(){
+    return artikli;
+  }
+  public Artikl izbaciArtiklSaKodom (String kod){
+    Artikl n=null;
+    for(int i = 0;i < artikli.length ; i++){
+      if(artikli[i].getKod().equals(kod)){
+        n=artikli[i];
+        artikli[i]=null;
+        break;
+      }
+    }
+    return n;
+  }
 }
+
